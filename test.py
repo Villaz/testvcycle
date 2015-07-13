16c22bbe4a59d9246ccebc848b1e535bbf8653b5
Villaz <villazonpersonal@gmail.com>
2015-07-13 17:16:56 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import stat
 import subprocess
 import base64
 
-open('/var/lib/waagent/script.sh', 'w').write(base64.b64decode(open(event.src_path, 'r').read()))
-os.chmod('/var/lib/waagent/script.sh', stat.S_IEXEC)
-subprocess.call(['dos2unix', '/var/lib/waagent/script.sh'])
-subprocess.call(['sh', '/var/lib/waagent/script.sh'])
+open('/var/lib/waagent/test.sh', 'w').write(base64.b64decode(open(event.src_path, 'r').read()))
+os.chmod('/var/lib/waagent/test.sh', stat.S_IEXEC)
+subprocess.call(['dos2unix', '/var/lib/waagent/test.sh'])
+subprocess.call(['sh', '/var/lib/waagent/test.sh'])
 os._exit(0)
