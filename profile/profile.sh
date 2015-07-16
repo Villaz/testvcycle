ed239f703fd874fb46e5f6c3808c38b4beb54653
Luis Villazon Esteban <villazonpersonal@gmail.com>
2015-07-16 16:23:10 +0200

    Added KV benchmark

--- profile/profile.sh
+++ profile/profile.sh
@@ -1,6 +1,46 @@
 #! /bin/bash
 
 
+function exec_kv {
+  mkdir /scratch/KV ; cd /scratch/KV
+  wget https://kv.roma1.infn.it/KV/sw-mgr --no-check-certificate -O sw-mgr
+  chmod u+x sw-mgr
+
+  export VO_ATLAS_SW_DIR=/cvmfs/atlas.cern.ch/repo/sw
+  echo 'source /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0/cmtsite/asetup.sh --dbrelease=current AtlasProduction 17.8.0.9 opt gcc46 slc6 64'
+  source /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0/cmtsite/asetup.sh --dbrelease=current AtlasProduction 17.8.0.9 opt gcc46 slc6 64
+
+
+  SW_MGR_START=`date +"%y-%m-%d %H:%M:%S"`
+  echo "start sw-mgr ${SW_MGR_START}"
+
+  wget $KVBMK -O /tmp/KVbmk.xml
+
+  KVSUITE=`grep -i "<kvsuite>" /tmp/KVbmk.xml | head -1 | sed -E "s@.*>(.*)<.*@\1@"`
+  echo KVBMK $KVBMK
+  echo KVSUITE $KVSUITE
+
+  echo "./sw-mgr -a 17.8.0.9-x86_64 --test 17.8.0.9 --no-tag -p /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0 --kv-disable ALL --kv-enable $KVSUITE --kv-conf $KVBMK --kv-keep --kvpost --kvpost-tag $KVTAG --tthreads $KVTHR "
+
+  REFDATE=`date +\%y-\%m-\%d_\%H-\%M-\%S`
+  KVLOG=kv_$REFDATE.out
+  ./sw-mgr -a 17.8.0.9-x86_64 --test 17.8.0.9 --no-tag -p /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0 --kv-disable ALL --kv-enable $KVSUITE --kv-conf $KVBMK --kv-keep --kvpost --kvpost-tag $KVTAG --tthreads $KVTHR > $KVLOG
+  [ $? -ne 0 ] && exit 1
+
+  TESTDIR=`ls -tr | grep kvtest_ | tail -1`
+  df -h > space_available.log
+  tar -cvjf ${TESTDIR}_${REFDATE}.tar.bz2 ${TESTDIR}/KV.thr.*/data/*/*log $KVLOG space_available.log
+  SW_MGR_STOP=`date +"%y-%m-%d %H:%M:%S"`
+  echo "end sw-mgr ${SW_MGR_STOP}"
+
+  PERFMONLOG=PerfMon_summary_`date +\%y-\%m-\%d_\%H:\%M:\%S`.out
+  echo "host_ip: `hostname`" >> $PERFMONLOG
+  echo "start sw-mgr ${SW_MGR_START}">> $PERFMONLOG
+  echo "end sw-mgr ${SW_MGR_STOP}" >> $PERFMONLOG
+  grep -H PerfMon $TESTDIR/KV.thr.*/data/*/*log >> $PERFMONLOG
+}
+
+
 #Install the packages needed to execute phoronix
 #If OS is CernVM the php is not compatible, it should be removed and installed again
 yum -y install epel-release
@@ -46,7 +86,12 @@ chmod u+w /home/phoronix/.phoronix-test-suite/user-config.xml
 
 #execute the tests
 sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/compress-7zip'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/encode-mp3'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/x264'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/build-linux-kernel'
 
+#execute KV Benchmark
+exec_kv
 
 
 #Parse the tets and send the information to DB or Message Broker
