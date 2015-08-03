e58902a5f999518c94d1f9fea6e6527f38414fc6
Luis Villazon Esteban <villazonpersonal@gmail.com>
2015-08-03 11:16:12 +0200

    Added benchmark

---
+++ benchmark/parser.py
@@ -0,0 +1,179 @@
+__author__ = 'Luis Villazon Esteban'
+
+import json
+import time
+import sys
+import commands
+from os import listdir
+import xml.etree.ElementTree as ET
+import argparse
+import ipgetter
+
+
+def extract_values(line):
+    """Extract the values from the line and return a dictionary with the value, error, and unit"""
+
+    values = line[line.find("(")+1:line.find(")")]
+    value = values[:values.find("+")].strip()
+    deviation = values[values.find("+/-")+3:].strip()
+    unit = line[line.find(")")+1:].strip()
+    if unit == 'ms':
+            value = '%.5f' % (float(value)/1000)
+            deviation = '%.5f' % (float(deviation)/1000)
+            unit = 's'
+    if float(deviation) == 0:
+        return {'value': float(value), 'unit':unit}
+    else:
+        return {'value': float(value), 'error': float(deviation), 'unit':unit}
+
+
+def fill_results(result, key, lines, i):
+    entries = lines[i][lines[i].find("=")+1:lines[i].find(")")].strip()
+    cpu = extract_values(lines[i+1])
+    real = extract_values(lines[i+2])
+    vmem = extract_values(lines[i+3])
+    result.update({key: {'entries': entries,
+                         'cpu': cpu,
+                         'real': real,
+                         'vmem': vmem
+                        }
+                   })
+
+
+def fill_memory_results(result, key, lines, i):
+    def extract_memory_value(line):
+        value = line[line.find("INFO")+len("INFO"):]
+        value = value[value.find(":")+2:].strip()
+        unit = value[value.find(" ")+1:].strip()
+        value = value[:value.find(" ")]
+        return {'value': float(value), 'unit':unit}
+
+    result.update({key:{ 'vm_data': extract_memory_value(lines[i+1]),
+                    'vm_exe': extract_memory_value(lines[i+2]),
+                    'VmHWM': extract_memory_value(lines[i+3]),
+                    'VmLck': extract_memory_value(lines[i+4]),
+                    'VmLib': extract_memory_value(lines[i+5]),
+                    'VmPTE': extract_memory_value(lines[i+6]),
+                    'VmPeak': extract_memory_value(lines[i+7]),
+                    'VmRSS': extract_memory_value(lines[i+8]),
+                    'VmSize': extract_memory_value(lines[i+9]),
+                    'VmStk': extract_memory_value(lines[i+10]),
+                    'VmSwap': extract_memory_value(lines[i+11])}})
+
+
+def fill_memory_leak_results(result, key, lines, i):
+    def extract_memory_value(line):
+        value = line[line.find("INFO")+len("INFO"):]
+        value = value[value.find(":")+2:].strip()
+        unit = value[value.find(" ")+1:].strip()
+        value = value[:value.find(" ")]
+        if unit == 'ms':
+            value = round(value/1000, 5)
+            unit = 's'
+        return {'value': float(value), 'unit':unit}
+        return float(value)
+    result.update({key: {'first-evt': extract_memory_value(lines[i+1]),
+                         '10th -evt':extract_memory_value(lines[i+2]),
+                         'last -evt':extract_memory_value(lines[i+3]),
+                         'evt  2-20':extract_memory_value(lines[i+4]),
+                         'evt 21-50':extract_memory_value(lines[i+5]),
+                         'evt 51+':extract_memory_value(lines[i+6])} })
+
+
+def parse_kv():
+    result = {'kv': {}}
+    path = "/scratch/KV"
+    file_name = None
+    for f in listdir(path):
+        if f.find("PerfMon_summary_") >= 0:
+            file_name = "%s/%s" % (path, f)
+            break
+
+    if file_name is None:
+        return result
+
+    file = open(file_name, "r")
+    lines = file.read().split("\n")
+    aux_result = None
+    for i in range(0, len(lines)):
+        line = lines[i]
+        if line.find("## PerfMonFlags ##")> 0:
+            thread = line[line.find("KV.thr.")+len("KV.thr.")]
+            result['kv'].update({"thr_"+thread:{}})
+            aux_result = result['kv']['thr_'+thread]
+        if line.find("Statistics for 'ini'") > 0:
+            fill_results(aux_result, 'initialization', lines, i)
+        if line.find("Statistics for 'evt'") > 0:
+            fill_results(aux_result, 'evt', lines, i)
+        if line.find("Statistics for 'fin'") > 0:
+            fill_results(aux_result, 'finalization', lines, i)
+        if line.find("memory infos from") > 0:
+            fill_memory_results(aux_result, 'memory', lines, i)
+        if line.find("vmem-leak estimation") > 0:
+            fill_memory_leak_results(aux_result, 'vmem-leak', lines, i)
+    return result
+
+
+def parse_phoronix():
+   path = '/home/phoronix/.phoronix-test-suite/test-results'
+   result = {'phoronix': {}}
+   for f in listdir(path):
+      if f.find('pts-results-viewer') < 0 and f[0] is not '.':
+         tree = ET.parse("%s/%s/%s" % (path, f, "test-1.xml"))
+         root = tree.getroot()
+         title = root.find('Result').find('Title').text
+         value = float(root.find('Result').find('Data').find('Entry').find('Value').text)
+         result['phoronix'].update({title: float(value)})
+   return result
+
+
+def parse_metadata(id, cloud, vo):
+    result = {'metadata':{}}
+    result.update({'_timestamp': int(time.time())})
+    result['metadata'].update({'ip': ipgetter.myip()})
+    result['metadata'].update({'cloud': cloud})
+    result['metadata'].update({'UID': id})
+    result['metadata'].update({'VO': vo})
+    result['metadata'].update({'osdist':commands.getoutput("lsb_release -d 2>/dev/null").split(":")[1][1:]})
+    result['metadata'].update({'pyver': sys.version.split()[0]})
+    result['metadata'].update({'cpuname': commands.getoutput("cat /proc/cpuinfo | grep '^model name' | tail -n 1").split(':')[1].lstrip()})
+    result['metadata'].update({'cpunum' : int(commands.getoutput("cat /proc/cpuinfo | grep '^processor' |wc -l"))})
+    result['metadata'].update({'bogomips': float(commands.getoutput("cat /proc/cpuinfo | grep '^bogomips' | tail -n 1").split(':')[1].lstrip())})
+    result['metadata'].update({'meminfo': float(commands.getoutput("cat /proc/meminfo | grep 'MemTotal:'").split()[1])})
+    return result
+
+
+def generate_rkv(document):
+    rkv = {}
+    for thread in document['profiles']['kv']:
+        for type in document['profiles']['kv'][thread]:
+            if type not in ['evt', 'initialization', 'finalization']: continue;
+            if type not in rkv:
+                rkv[type] = {}
+            for metric in document['profiles']['kv'][thread][type]:
+                if metric == 'entries' or metric not in ['cpu', 'real']: continue;
+                if "%s_values" % metric not in rkv[type]:
+                    rkv[type]["%s_values" % metric] = []
+                rkv[type]["%s_values" % metric].append(document['profiles']['kv'][thread][type][metric]['value'])
+    return rkv
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--id", nargs='?', help="VM identifier")
+    parser.add_argument("-v", "--vo", nargs='?', help="VO")
+    parser.add_argument("-c", "--cloud", nargs='?', help="Cloud")
+    args = parser.parse_args()
+
+
+    result = parse_metadata(args.id, args.cloud, args.vo)
+    result.update({'profiles': {}})
+    result['profiles'].update(parse_phoronix())
+    result['profiles'].update(parse_kv())
+    result['profiles'].update({'rkv': generate_rkv(result)})
+
+    file = "/tmp/result_profile.json"
+    open(file,'w').write(json.dumps(result))
+
+
---
+++ benchmark/profile.sh
@@ -0,0 +1,188 @@
+#!/usr/bin/env bash
+
+function dump_kv_file(){
+cat > /tmp/KVbmk.xml << 'EOF'
+<?xml version="1.0"?>
+<!DOCTYPE unifiedTestConfiguration SYSTEM "http://www.hep.ucl.ac.uk/atlas/AtlasTesting/DTD/unifiedTestConfiguration.dtd">
+
+<unifiedTestConfiguration>
+
+<kv>
+    <kvtest name='AtlasG4SPG' enabled='true'>
+      <release>ALL</release>
+      <priority>20</priority>
+      <kvsuite>KV2012</kvsuite>
+      <trf>AtlasG4_trf.py</trf>
+      <desc>Single Muon Simulation</desc>
+      <author>Alessandro De Salvo [Alessandro.DeSalvo@roma1.infn.it]</author>
+      <outpath>${T_DATAPATH}/SimulHITS-${T_RELEASE}</outpath>
+      <outfile>${T_PREFIX}-SimulHITS-${T_RELEASE}.pool.root</outfile>
+      <logfile>${T_PREFIX}-SimulHITS-${T_RELEASE}.log</logfile>
+      <kvprestage>http://kv.roma1.infn.it/KV/input_files/simul/preInclude.SingleMuonGenerator.py</kvprestage>
+      <signature>
+        outputHitsFile="${T_OUTFILE}" maxEvents=100 skipEvents=0 preInclude=KitValidation/kv_reflex.py,preInclude.SingleMuonGenerator.py geometryVersion=ATLAS-GEO-16-00-00 conditionsTag=OFLCOND-SDR-BS7T-04-03
+      </signature>
+      <copyfiles>
+        ${T_OUTFILE} ${T_LOGFILE} PoolFileCatalog.xml metadata.xml jobInfo.xml
+      </copyfiles>
+      <checkfiles>${T_OUTPATH}/${T_OUTFILE}</checkfiles>
+    </kvtest>
+</kv>
+</unifiedTestConfiguration>
+EOF
+}
+
+function kv(){
+KVBMK="file:///tmp/KVbmk.xml"
+KVTAG=$SITE
+KVTHR=`grep -c processor /proc/cpuinfo`
+
+rm -rf /scratch/KV/
+mkdir -p /scratch/KV ; cd /scratch/KV
+wget https://kv.roma1.infn.it/KV/sw-mgr --no-check-certificate -O sw-mgr
+chmod u+x sw-mgr
+
+export VO_ATLAS_SW_DIR=/cvmfs/atlas.cern.ch/repo/sw
+echo 'source /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0/cmtsite/asetup.sh --dbrelease=current AtlasProduction 17.8.0.9 opt gcc46 slc6 64'
+source /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0/cmtsite/asetup.sh --dbrelease=current AtlasProduction 17.8.0.9 opt gcc46 slc6 64
+
+
+SW_MGR_START=`date +"%y-%m-%d %H:%M:%S"`
+echo "start sw-mgr ${SW_MGR_START}"
+
+KVSUITE=`grep -i "<kvsuite>" /tmp/KVbmk.xml | head -1 | sed -E "s@.*>(.*)<.*@\1@"`
+echo KVBMK $KVBMK
+echo KVSUITE $KVSUITE
+
+echo "./sw-mgr -a 17.8.0.9-x86_64 --test 17.8.0.9 --no-tag -p /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0 --kv-disable ALL --kv-enable $KVSUITE --kv-conf $KVBMK --kv-keep --kvpost --kvpost-tag $KVTAG --tthreads $KVTHR "
+
+REFDATE=`date +\%y-\%m-\%d_\%H-\%M-\%S`
+KVLOG=kv_$REFDATE.out
+./sw-mgr -a 17.8.0.9-x86_64 --test 17.8.0.9 --no-tag -p /cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc6-gcc46-opt/17.8.0 --kv-disable ALL --kv-enable $KVSUITE --kv-conf $KVBMK --kv-keep --kvpost --kvpost-tag $KVTAG --tthreads $KVTHR > $KVLOG
+
+TESTDIR=`ls -tr | grep kvtest_ | tail -1`
+df -h > space_available.log
+tar -cvjf ${TESTDIR}_${REFDATE}.tar.bz2 ${TESTDIR}/KV.thr.*/data/*/*log $KVLOG space_available.log
+SW_MGR_STOP=`date +"%y-%m-%d %H:%M:%S"`
+echo "end sw-mgr ${SW_MGR_STOP}"
+
+PERFMONLOG=PerfMon_summary_`date +\%y-\%m-\%d_\%H:\%M:\%S`.out
+echo "host_ip: `hostname`" >> $PERFMONLOG
+echo "start sw-mgr ${SW_MGR_START}">> $PERFMONLOG
+echo "end sw-mgr ${SW_MGR_STOP}" >> $PERFMONLOG
+grep -H PerfMon $TESTDIR/KV.thr.*/data/*/*log >> $PERFMONLOG
+}
+
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --id=*    )             ID=${1#*=};
+                                ;;
+        --cloud=* )             CLOUD=${1#*=};
+                                ;;
+        --vo=* )                VO=${1#*=};
+                                ;;
+        --queue_port=* )        QUEUE_PORT=${1#*=};
+                                ;;
+        --queue_host=* )        QUEUE_HOST=${1#*=};
+                                ;;
+        --username=* )          QUEUE_USERNAME=${1#*=};
+                                ;;
+        --password=* )          QUEUE_PASSWORD=${1#*=};
+                                ;;
+        --topic=* )             QUEUE_NAME=${1#*=};
+                                ;;
+        * )         echo -e "${usage}" >&3
+                    exit 1
+    esac
+    shift
+done
+
+
+#Install pip and python enviroment
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+#create python enviroment
+pip install virtualenv
+mkdir -p /usr/python-env
+virtualenv /usr/python-env/
+source /usr/python-env/bin/activate
+pip install wheel
+pip install requests
+pip install python-geoip
+pip install python-geoip-geolite2
+pip install ipgetter
+pip install stomp.py
+deactivate
+
+#Install the packages needed to execute phoronix
+#If OS is CernVM, php is not compatible, it should be removed and installed again
+yum -y install epel-release
+yum -y install lapack fio gcc-c++ yasm
+
+kernel=`uname -r`
+if [[ $kernel == *"cernvm"* ]]; then
+  yum -y remove php
+fi
+yum -y install php
+yum -y install php-domxml
+
+
+if ! hash phoronix-test-suite 2>/dev/null; then
+  #Downloads and install phoronix
+  cd /root
+  wget http://www.phoronix-test-suite.com/download.php?file=phoronix-test-suite-5.8.1 -O phoronix-test-suite-5.8.1.tar.gz
+  tar -zxf phoronix-test-suite-5.8.1.tar.gz
+  cd /root/phoronix-test-suite ; ./install-sh
+fi
+
+
+#Creates a new user to execute the tests
+#If the tests are executed as root, the user-config and the result tests won't be well defined.
+/usr/sbin/useradd -b /home phoronix
+echo phoronix | passwd phoronix --stdin
+
+sed -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config > /etc/ssh/sshd_config_b
+mv -f /etc/ssh/sshd_config_b /etc/ssh/sshd_config
+service sshd restart
+
+#Configure phoronix
+yum -y install sshpass
+
+cat > /tmp/download.py << 'EOF'
+import requests
+from requests.adapters import HTTPAdapter
+
+s = requests.Session()
+s.mount('http://vcycle-manager-lv.cern.ch', HTTPAdapter(max_retries=5))
+r = s.get('http://vcycle-manager-lv.cern.ch/scripts/phoronix.tar.gz', stream=True)
+with open('/home/phoronix/phoronix.tar.gz', 'wb') as f:
+    for chunk in r.iter_content(chunk_size=1024):
+        if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+            f.flush()
+EOF
+chmod ugo+rx /tmp/download.py
+
+#download phoronix data and execute the tests
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'source /usr/python-env/bin/activate; python /tmp/download.py; deactivate; cd /home/phoronix/; tar -zxvf /home/phoronix/phoronix.tar.gz'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/compress-7zip'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/encode-mp3'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/x264'
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 'phoronix-test-suite batch-run pts/build-linux-kernel'
+
+#execute KV Benchmark
+dump_kv_file
+kv
+
+#Parse the tests
+cat <<X5_EOF >/tmp/parser
+source /usr/python-env/bin/activate
+python $PWD/parser.py -i $ID -c $CLOUD -v $VO
+python $PWD/send.py -p $QUEUE_PORT -s $QUEUE_HOST -u $QUEUE_USERNAME -p $QUEUE_PASSWORD -n $QUEUE_NAME -f /tmp/result_profile.json
+deactivate
+X5_EOF
+chmod ugo+rx /tmp/parser
+
+sshpass -p "phoronix" ssh -o StrictHostKeyChecking=no phoronix@127.0.0.1 "/tmp/parser"
