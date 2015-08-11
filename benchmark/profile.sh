30dff545196db3c7fee42eccb39afcc89f155018
Villaz <villazonpersonal@gmail.com>
2015-08-11 18:16:20 +0200

    Commented send_queue to test propose

--- benchmark/profile.sh
+++ benchmark/profile.sh
@@ -193,7 +193,7 @@ source /usr/python-env/bin/activate
 source /tmp/times.source
 source /usr/share/sources
 python /var/spool/checkout/testvcycle/benchmark/parser.py -i $ID -c $CLOUD -v $VO
-python /var/spool/checkout/testvcycle/benchmark/send_queue.py --port=$QUEUE_PORT --server=$QUEUE_HOST --username=$QUEUE_USERNAME --password=$QUEUE_PASSWORD --name=$QUEUE_NAME --file=/tmp/result_profile.json
+#python /var/spool/checkout/testvcycle/benchmark/send_queue.py --port=$QUEUE_PORT --server=$QUEUE_HOST --username=$QUEUE_USERNAME --password=$QUEUE_PASSWORD --name=$QUEUE_NAME --file=/tmp/result_profile.json
 deactivate
 X5_EOF
 chmod ugo+rx /tmp/parser
